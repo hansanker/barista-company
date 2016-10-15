beda23ff5d5f6f173ff6852801d2b3ad14d49f3b
Hans Anker <ankersupport@gmail.com>
2016-10-15 22:06:55 +0200

    add firebase and AF2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { MaterializeModule } from 'angular2-materialize';
+import { AngularFireModule } from 'angularfire2';
 
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,15 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { BackofficeComponent } from './backoffice/backoffice.component';
 import { AuthComponent } from './auth/auth.component';
 import { ChecklistFormComponent } from './checklist/checklist-form/checklist-form.component';
+
+  // Initialize Firebase
+  export const firebaseConfig = {
+    apiKey: "AIzaSyCWYY3poe4SpATY-tx2FAIaou-ToD2I1wM",
+    authDomain: "baristacompany-b618b.firebaseapp.com",
+    databaseURL: "https://baristacompany-b618b.firebaseio.com",
+    storageBucket: "baristacompany-b618b.appspot.com",
+    messagingSenderId: "623851489264"
+  };
 
 @NgModule({
   declarations: [
@@ -26,7 +36,8 @@ import { ChecklistFormComponent } from './checklist/checklist-form/checklist-for
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterializeModule
+    MaterializeModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
