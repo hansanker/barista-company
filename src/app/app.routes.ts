26e0cb9e65a09c5d4147a114827e9b5d0faf823b
Ann Prokhorova <prokhorova.ann26@gmail.com>
2016-12-18 02:41:08 +0400

    fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const AppRoutes = [
         component: ChecklistBaristaComponent,
       },
       {
-        path: 'checklistForm',
+        path: 'checklist/checklistForm',
         component: ChecklistFormComponent
       },
       {
